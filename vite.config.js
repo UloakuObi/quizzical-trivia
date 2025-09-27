434a92857af37d6e2383472757a71bd420212916
UloakuObi <uloakuobi@yahoo.com>
2025-09-27 13:37:28 +0100

    Initial project setup

---
+++ vite.config.js
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    open: true    // auto-open browser when dev server starts
+  }
+})
+
+
