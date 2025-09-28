1f20fe149bb8572fc85b088157e6052b589bd233
UloakuObi <140077049+UloakuObi@users.noreply.github.com>
2025-09-28 14:30:38 +0100

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/quizzical-trivia/',
   server: {
     port: 5173,
     open: true    // auto-open browser when dev server starts
