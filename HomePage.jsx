846548736c3bfc72c512c0570d24b5b4669010bd
UloakuObi <140077049+UloakuObi@users.noreply.github.com>
2025-09-28 14:52:05 +0100

    Update HomePage.

--- HomePage.jsx
+++ HomePage.jsx
@@ -1,11 +1,14 @@
+import yellowBlob from "./images/blob-yellow.png"
+import blueBlob from "./images/blob-blue.png"
+
 export default function HomePage(props) {
     return (
         <div className="home-page"> 
-            <img className="yellow-blob" src="/images/blob-yellow.png" alt="decorative background image"/>
+            <img className="yellow-blob" src={yellowBlob} alt="decorative background image"/>
             <h1>Quizzical</h1>
             <p>Test your brainpower with quick-fire trivia!</p>
             <button className="start-quiz" onClick={props.fetchQuiz}>Start Quiz</button>
-            <img className="blue-blob" src="/images/blob-blue.png" alt="decorative background image"/>
+            <img className="blue-blob" src={blueBlob} alt="decorative background image"/>
         </div>
     )
 }
