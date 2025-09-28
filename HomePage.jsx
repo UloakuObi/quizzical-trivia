1ee04eebd3255e1cc8e8a901a542f958d395a02e
UloakuObi <140077049+UloakuObi@users.noreply.github.com>
2025-09-28 14:26:12 +0100

    Update HomePage

--- HomePage.jsx
+++ HomePage.jsx
@@ -1,11 +1,11 @@
 export default function HomePage(props) {
     return (
         <div className="home-page"> 
-            <img className="yellow-blob" src="https://raw.githubusercontent.com/UloakuObi/quizzical-trivia/main/blob-yellow.png"/>
+            <img className="yellow-blob" src="images/blob-yellow.png" alt="decorative background image"/>
             <h1>Quizzical</h1>
             <p>Test your brainpower with quick-fire trivia!</p>
             <button className="start-quiz" onClick={props.fetchQuiz}>Start Quiz</button>
-            <img className="blue-blob" src="https://raw.githubusercontent.com/UloakuObi/quizzical-trivia/main/blob-blue.png"/>
+            <img className="blue-blob" src="images/blob-blue.png" alt="decorative background image"/>
         </div>
     )
 }
